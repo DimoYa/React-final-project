d6b943d16b2b0df2b6bf4952333174dbaa3dc96f
Dimo Yanev <Dimo_Yanev@abv.bg>
2022-07-11 21:21:10 +0300

    add styles

--- src/more-for-fishing/src/components/common/header/Header.js
+++ src/more-for-fishing/src/components/common/header/Header.js
@@ -7,39 +7,39 @@ export const Header = () => {
                 <div className="navbar-collapse collapse d-sm-inline-flex justify-content-between" >
                     <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                         <li className="nav-item" href="/">
-                            <a><i className="fa fa-fw fa-home"></i><span>Home</span></a>
+                            <p><i className="fa fa-fw fa-home"></i><span>Home</span></p>
                         </li>
                         <li className="nav-item" href="#">
-                            <a><i className="fa fa-sign-in-alt"></i><span>Login</span></a>
+                            <p><i className="fa fa-sign-in-alt"></i><span>Login</span></p>
                         </li>
                         <li className="nav-item" href="#">
-                            <a><i className="fas fa-user-plus"></i><span>Register</span></a>
+                            <p><i className="fas fa-user-plus"></i><span>Register</span></p>
                         </li>
                         <li className="nav-item" href="#">
-                            <a><i className="fa fa-plus"></i><span>Create
-                                article</span></a>
+                            <p><i className="fa fa-plus"></i><span>Create
+                                article</span></p>
                         </li>
                         <li className="nav-item" href="#">
-                            <a><i className="fas fa-newspaper"></i><span>My articles</span></a>
+                            <p><i className="fas fa-newspaper"></i><span>My articles</span></p>
                         </li>
                         <li className="nav-item" href="#">
-                            <a><i className="fas fa-person-booth"></i><span>All articles</span></a>
+                            <p><i className="fas fa-person-booth"></i><span>All articles</span></p>
                         </li>
                         <li className="dropdown">
-                            <a className="dropdown-toggle" data-toggle="dropdown" aria-expanded="true"><i
-                                className="fas fa-user-shield"></i>Admin</a>
+                            <p className="dropdown-toggle" data-toggle="dropdown" aria-expanded="true"><i
+                                className="fas fa-user-shield"></i>Admin</p>
                             <ul className="dropdown-menu">
-                                <a className="dropdownItem" href="#">User management</a>
+                                <p className="dropdownItem" href="#">User management</p>
                             </ul>
                         </li>
                     </ul>
                     <ul className="navbar-nav mt-2 mt-lg-0">
 
                         <li className="nav-item" href="#">
-                            <a><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" className="avatar" alt="Avatar" /></a>
+                            <p><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" className="avatar" alt="Avatar" /></p>
                         </li>
                         <li className="nav-item">
-                            <a><i className="fas fa-sign-out-alt"></i><span>Logout</span></a>
+                            <p><i className="fas fa-sign-out-alt"></i><span>Logout</span></p>
                         </li>
                     </ul>
                 </div>
