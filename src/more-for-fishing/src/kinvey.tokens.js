92a75eb7ef750787d0a82f4b6b4fd5d7d5006d36
Dimo Yanev <Dimo_Yanev@abv.bg>
2022-08-24 21:12:40 +0300

    add new kinvey BE and redeploy

--- src/more-for-fishing/src/kinvey.tokens.js
+++ src/more-for-fishing/src/kinvey.tokens.js
@@ -1,3 +1,3 @@
-export const appKey = 'kid_BykKojWnq';
-export const appSecret = 'ce19b2cd99d44eef8852147d4e341f50';
-export const appMasterSecret = 'a1f18a709919483593351044e5a900d1';
+export const appKey = 'kid_H1QiVkEyi';
+export const appSecret = 'fa716775b3d341a48e736e73a161a76a';
+export const appMasterSecret = '88e2b61c9fd14af2804e5c965737ce1a';
