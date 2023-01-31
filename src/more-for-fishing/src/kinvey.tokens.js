9b9efbd30df35eb17fc7d4afd709f72e8d3e8dbd
Dimo Yanev <Dimo_Yanev@abv.bg>
2023-02-01 00:03:04 +0200

    update be key

--- src/more-for-fishing/src/kinvey.tokens.js
+++ src/more-for-fishing/src/kinvey.tokens.js
@@ -1,3 +1,3 @@
-export const appKey = 'kid_H1vewY-Us';
-export const appSecret = 'ebb0a613bf05462d8a26cdbbb0e9cac9';
-export const appMasterSecret = '17a4b925bd6840b4a1550f219b9b61ea';
+export const appKey = 'kid_ryG11fv3i';
+export const appSecret = 'd5bd275223b3469c8f5fc250e9b7f4aa';
+export const appMasterSecret = 'e5a09febdf504ec1a9fa60055308f311';
