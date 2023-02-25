2bd7f6b67c35322dec241a04afeaf9a9f91b5dd5
Dimo Yanev <dyanev@kpmg.com>
2023-02-25 22:40:59 +0200

    add rendering and user interaction unit tests

---
+++ src/more-for-fishing/src/components/features/article/article-create/ArticleCreate.test.js
@@ -0,0 +1,42 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import React from 'react';
+import { ArticleCreate } from './ArticleCreate';
+import { MemoryRouter as Router } from 'react-router-dom';
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
+import { AuthProvider } from '../../../../context/AuthContext';
+import { toast } from 'react-toastify'
+import { act } from 'react-dom/test-utils'
+
+// example of user interaction
+describe('Article create component', () => {
+    it('should submit new article', async () => {
+
+        jest.mock('react-toastify', () => ({
+            toast: {
+                success: jest.fn(),
+            },
+        }))
+
+        act(async () => {
+            const { container } = render(
+                <Router>
+                    <AuthProvider>
+                        <ArticleCreate />
+                    </AuthProvider>
+                </Router>
+            );
+            const titleInput = container.getElementsByTagName('input')[0];
+            await userEvent.type(titleInput, 'New Article');
+
+            const contntInput = container.getElementsByTagName('textarea')[0];
+            await userEvent.type(contntInput, 'New Article text');
+
+            const submitBtn = container.getElementsByTagName('button')[1];
+
+            await userEvent.click(submitBtn);
+
+            expect(toast.success).toBeDefined();
+        })
+    });
+});
---
+++ src/more-for-fishing/src/components/features/article/article-item/ArticleItem.test.js
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { ArticleItem } from './ArticleItem';
+import { MemoryRouter as Router } from 'react-router-dom';
+import '@testing-library/jest-dom/extend-expect';
+
+// examle of rendering component tets
+describe('Article item component', () => {
+    it('should render article item', () => {
+
+        const articleData = {
+            headline: 'test headline',
+            author: 'test author',
+            content: 'tst article content',
+            _kmd: {
+                "lmt": "2023-02-01T22:38:18.719Z",
+                "ect": "2023-02-01T22:38:18.719Z"
+            }
+        }
+
+        const { container } = render(
+            <Router>
+                <ArticleItem article={articleData} />
+            </Router>
+        );
+
+        const articleTitle = container.getElementsByTagName('h4')[0];
+        const articleAuthor = container.getElementsByClassName('fa fa-pencil p-1')[0];
+
+
+        expect(articleTitle).toBeInTheDocument();
+        expect(articleTitle).toHaveTextContent(articleData.headline);
+        expect(articleAuthor).toBeInTheDocument();
+        expect(articleAuthor).toHaveTextContent(articleData.author);
+    });
+});
